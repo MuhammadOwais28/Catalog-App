82d01619d05e9d5401b867d7feb35c93fbfd962f
Muhammad Owais Amin <owaisamin862@gmail.com>
2022-10-06 21:23:50 +0500

    Day 2 completed.

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var days = 30;
+    var name = "Flutter";
+    return Scaffold(
+      appBar: AppBar(title: Text("Catalog App")),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of $name."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
