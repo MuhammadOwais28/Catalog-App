b2994c64234c4fffaa78a0f38e94c5e9587450df
Muhammad Owais Amin <owaisamin862@gmail.com>
2022-10-14 01:59:58 +0500

    "day 12: learned about creating listview of catalog"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var days = 30;
-    var name = "Flutter";
     return Scaffold(
       appBar: AppBar(title: const Text("Catalog App")),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of $name."),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
