2179a01e92a5c583842de8d6a78e5f1ccc649a3a
Muhammad Owais Amin <owaisamin862@gmail.com>
2022-10-22 23:36:20 +0500

    "day 13 (learned to convert json string to json object)"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'dart:convert';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json"); //loaded json string 
+    final decodedData = jsonDecode(catalogJson);  //decoded json string to json object, return "product"
+    // in which 8 lists are available
+    final productsData = decodedData["products"]; //took out 8 lists directly from products 
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
