09a6e7789fb3b411efbfa7d16d4897341aa56075
Muhammad Owais Amin <owaisamin862@gmail.com>
2022-10-31 21:47:31 +0500

    "day 16 completed (learned about gridview)"

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,12 +43,37 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16.0,
+                    crossAxisSpacing: 12.0),
                 itemCount: CatalogModel.items.length,
                 itemBuilder: (context, index) {
-                  return ItemWidget(
-                    item: CatalogModel.items[index],
-                  );
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      child: GridTile(
+                        header: Container(
+                          child: Text(
+                            item.name,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.green),
+                        ),
+                        child: Image.network(item.image),
+                        footer: Container(
+                          child: Text(
+                            item.price.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(color: Colors.black),
+                        ),
+                      ));
                 },
               )
             : Center(
@@ -56,7 +81,6 @@ class _HomePageState extends State<HomePage> {
               ),
       ),
       drawer: MyDrawer(),
-      
     );
   }
 }
